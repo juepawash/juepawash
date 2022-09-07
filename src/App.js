6b1d0eb5b714933c24e0f3edcc6d14bce6c0c839
juepawash <juepawash@gmail.com>
2022-09-06 19:18:40 -0500

    se agrega el QR

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import QRCode from "react-qr-code";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <QRCode value="https://www.google.com/"></QRCode>
         <p>
-         Hola mundo en react lavadero 0.0
+        Esta QR te manda para www.google.com prueba de escanner 
         </p>
         <a
           className="App-link"
